71f9e5a1114d1c89d33b609e72705b5e609063fe
Ko Nishiyama <nishiyama.ko.0420@gmail.com>
2020-09-21 15:40:41 +0900

    First_Commit

---
+++ src/lib/firebase.ts
@@ -0,0 +1,24 @@
+import * as firebase from "firebase";
+import "firebase/firestore";
+import { Shop } from "../types/shop";
+import Constants from "expo-constants";
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(Constants.manifest.extra.firebase);
+}
+
+export const getShops = async () => {
+  try {
+    const snapshot = await firebase
+      .firestore()
+      .collection("shops")
+      // .where("place", "==", "品川")
+      .orderBy("score", "desc")
+      .get();
+    const shops = snapshot.docs.map((doc) => doc.data() as Shop);
+    return shops;
+  } catch (err) {
+    console.log(err);
+    return [];
+  }
+};
---
+++ src/screens/HomeScreen.tsx
@@ -0,0 +1,54 @@
+import React, { useEffect, useState } from "react";
+import { StyleSheet, Text, View, FlatList, SafeAreaView } from "react-native";
+import { Shop } from "../types/shop";
+import { ShopReviewItem } from "../components/ShopReviewItem";
+import { getShops } from "../lib/firebase";
+import { StackNavigationProp } from "@react-navigation/stack/lib/typescript/src/types";
+import { RootStackParamList } from "../types/navigation";
+
+type Props = {
+  navigation: StackNavigationProp<RootStackParamList, "Home">;
+};
+
+export const HomeScreen = ({ navigation }: Props) => {
+  const [shops, setShops] = useState<Shop[]>([]);
+
+  useEffect(() => {
+    getFirebaseItems();
+  }, []);
+
+  const getFirebaseItems = async () => {
+    const shops = await getShops();
+    setShops(shops);
+  };
+
+  // const shopItems = shops.map((shop, index) => (
+  //   <ShopReviewItem shop={shop} key={index.toString()} />
+  // ));
+
+  const onPressShop = (shop: Shop) => {
+    navigation.navigate("Shop", { shop });
+  };
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={shops}
+        renderItem={({ item }: { item: Shop }) => (
+          <ShopReviewItem shop={item} onPress={() => onPressShop(item)} />
+        )}
+        keyExtractor={(item, index) => index.toString()}
+        numColumns={2}
+      />
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
